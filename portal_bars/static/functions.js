a3734ac53002bbb7c773e6f13b01b403222d5249
Kirill Bobrov <krllbobrov@gmail.com>
2023-03-22 10:55:02 +0300

    жс файл

---
+++ portal_bars/static/functions.js
@@ -0,0 +1,11 @@
+export function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("btn☰").style.opacity = "0";
+
+}
+
+export function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("btn☰").style.opacity = "1";
+}
+
